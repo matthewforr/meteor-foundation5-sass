c19a674d06584f32a60d55328a340d827977fefb
Li Chuangbo <im@chuangbo.li>
2014-08-29 15:54:46 +1200

    add fastclick

--- package.js
+++ package.js
@@ -11,6 +11,7 @@ Package.on_use(function (api) {
 
   api.add_files([
     "js/vendor/modernizr.js",
+    "js/vendor/fastclick.js",
     "js/foundation/foundation.js",
     "js/foundation/foundation.abide.js",
     "js/foundation/foundation.accordion.js",
